33601bd5dda04f57756d6338ad292ad99087f87c
robertnowell <robert.j.nowell@gmail.com>
2020-03-18 10:18:42 -0700

    questionable2scount

---
+++ misc/2scount.py
@@ -0,0 +1,29 @@
+def count2sAtD(n, d):
+	powerOfTen = 10**d
+	nextPow = powerOfTen**10
+	right = n % powerOfTen
+
+	roundDown = n - n % nextPow
+	roundUp = roundDown + nextPow
+	digit = (n / powerOfTen) % 10
+	if (digit < 2):
+		return roundDown /10
+	if digit == 2:
+		return roundDown/10 + right + 1
+	else:
+		return roundUp/10
+
+def count2s(n):
+	count = 0
+	length = len(str(n))
+	for i in range(length):
+		count += count2sAtD(n, i)
+	return count
+
+
+if __name__ == '__main__':
+	print(count2s(10))
+	print(count2s(100))
+	print(count2s(1200))
+	print(count2s(125))
+
